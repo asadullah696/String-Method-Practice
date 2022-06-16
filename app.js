ab156351ffe35d4849c0f31cf356365a158d3e93
asadullah696 <mr.asadullah19@gmail.com>
2022-06-16 23:32:45 +0500

    Main Java file for String Method Practice

---
+++ app.js
@@ -0,0 +1,453 @@
+
+// // // Question no 1 
+// // var a = prompt("Enter your First name ");
+// // var b = prompt("Enter your Second name ");
+// // var fullName = (a  + b);
+// // document.write("<h1>" + "Asalam-o-Alaikum " + fullName, + "</h1>" + "<br>");
+
+
+// // // Question no 2
+// // var c = prompt("Enter your favorite mobile phone with model");
+// // document.write("Your favorite phone is: " + c + "<br>");
+// // // document.write("Length of String:" + mobile.length, "<br>");
+
+// // // Question no 3
+// // var text = prompt("Enter your Nationality");
+// // for (i = 1; i < text.length; i++){
+// //     if (text.charAt(i) === "n"){
+// //         document.write("String:" + text, "<br>");
+// //         document.write("index of 'n': " + i, "<br>");
+// //     }
+// // }
+
+
+
+// // // Question no 5
+// // var index = text;
+// // var index2 = index.charAt(3);
+// // document.write("String:" + index, "<br>")
+// // document.write("Character at index 3:" + index2, "<br>");
+
+
+
+// // // Question no 6
+// // var firstName = a;
+// // var lastName = b;
+// // var fullName = firstName + lastName;
+// // document.write(fullName + " FullName using string concat() method.", "<br>");
+
+
+
+// // // Question no 7
+// // var city = prompt("Enter your city,");
+// // var city2 = city.replace(city, "Islamabad");
+// // document.write("City: " + city, "<br>");
+// // document.write("After replacement: " + city2, "<br>");
+
+
+
+// // // Question no 8
+// // var bestFriend = prompt("Enter your best friends Name.")
+// // var message = a + " and " + bestFriend + " are best friends. They play cricket and football together.";
+// // var message2 = message.replace(/and/g, "&");
+// // document.write("Message: " + message, "<br>");
+// // document.write("After replacement: " + message2, "<br>");
+
+
+
+
+// // // Question no 9
+// // var no1 = "123";
+// // var no2 = parseInt(no1);
+// // document.write(no1.value(), "<br>");
+// // document.write(no1.type(), "<br>");
+// // document.write(no2.value(), "<br>");
+// // document.write(no2.type(), "<br>");
+
+
+
+// // // Question no 10
+// // var url = prompt("Enter eb url. e.g: www.google.com");
+// // var url2 = "";
+// // document.write(url2);
+
+
+
+// // // Question no 11
+// // var fruit = prompt("Enter your favorite Fruit name");
+// // document.write("User input: " + fruit, "<br>");
+// // var fruit2 = fruit.toUpperCase();
+// // document.write("UpperCase: " + fruit2, "<br>");
+
+
+
+// // // Question no 12
+// // var dryFruit = prompt("Enter your favorite DryFruit name in UpperCase format");
+// // document.write("User input: " + dryFruit, "<br>");
+// // var dryFruit2 = dryFruit.toLowerCase();
+// // document.write("LowerCase: " + dryFruit2, "<br>");
+
+
+
+
+// // // Question no 13
+// // var a = prompt("Write any Number");
+// // var b = parseInt(a);
+// // alert(b);
+
+
+
+// // // Question no 14
+// // var number = 35.36;
+// // var number2 = number.toString();
+// // document.write("Number: " + number, "<br>");
+// // document.write("Result: " + number2 * 100, "<br>");
+
+
+
+// // // Question no 15
+// // var x = "4";
+// // var y = "4";
+// // var z = x + y;
+// // document.write("x is " + x, "<br>");
+// // document.write("y is " + y, "<br>");
+// // document.write("x + y is " + z, "<br>");
+
+
+
+// // // Question no 16
+// // var d = "9";
+// // var e = "9";
+// // var f = parseInt(d) - parseInt(e);
+// // document.write("d is " + d, "<br>");
+// // document.write("e is " + e, "<br>");
+// // document.write("d - e is " + f, "<br>");
+
+
+
+
+// // document.write(" My name is ASADULLAH. \n Roll No: KH01210812951 \n This is my 2nd Assignment..")
+
+
+// // var a=Number(prompt("Write any number to check, its divisible by 3 or not...."));
+
+// // if( a % 3 == 0) {
+// //     alert("The number is divisible by three.");
+// // }
+// // else if( a % 3 !== 0){
+// //     alert("This Number is not divisible by 3");
+// // }
+
+// // var b=Number(prompt("Write any number to check, the number is even or odd...."));
+// // if( b % 2 ==0 ){
+// //     alert("The number is even..")
+// // }
+// // else if( b% 2 !==0 ){
+// //     alert("The number is odd..")
+// // }
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// document.write("<h3>"+ "Assalam O Alaikum! " + firstName + " " + lastName + "</h3>");
+
+// var age=Number(prompt("Enter your age to continue"));
+// if(age>=18){
+//     document.write("<h6>" + "Old Enough" + "</h6>");
+// }
+// else if(age<18){
+//     document.write("<h6>" + "Too Young" + "</h6>");
+// }
+
+// var text = prompt("Enter your Nationality");
+// for (i = 1; i < text.length; i++){
+//     if (text.charAt(i) === "n"){
+//         document.write("String:" + text, "<br>");
+//         document.write("index of 'n': " + i, "<br>");
+//     }
+// }
+// var city = prompt("Enter your city,");
+// var city2 = city.replace(city, "Islamabad");
+// document.write("City: " + city, "<br>");
+// document.write("After replacement: " + city2, "<br>");
+
+// var bestFriend = prompt("Enter your best friends Name.")
+// var message = (a + " and " + bestFriend + " are best friends. They play cricket and football together.");
+// // var message2 = message.replace(/and/g, "&");
+// document.write( message + "<br>");
+// // document.write("After replacement: " + message2, "<br>");
+
+// // var no1 = "123";
+// // var no2 = parseInt(no1);
+// // document.write(no1.value(), "<br>");
+// // document.write(no1.type(), "<br>");
+// // document.write(no2.value(), "<br>");
+// // document.write(no2.type(), "<br>");
+
+// var url = prompt("Enter eb url. e.g: www.google.com");
+// var url2 = "";
+// document.write(url2);
+
+// var fruit = prompt("Enter your favorite Fruit name");
+// document.write("User input: " + fruit, "<br>");
+// var fruit2 = fruit.toUpperCase();
+// document.write("UpperCase: " + fruit2, "<br>");
+
+// var dryFruit = prompt("Enter your favorite DryFruit name in UpperCase format");
+// document.write("User input: " + dryFruit, "<br>");
+// var dryFruit2 = dryFruit.toLowerCase();
+// document.write("LowerCase: " + dryFruit2, "<br>");
+
+
+
+// // alert("Give Two Value In text input Boxes to Add these two Numbers..")
+
+// // var a1=Number(prompt("Write Any Number as the value of a"));
+// // var b2=Number(prompt("Write Any Number as the value of b"));
+// // var a=a1;
+// // var b=b2;
+// // var sum= a+b;
+// // alert("The Sum of " + a + " and " + b +" is equal to " + sum );
+
+// // console.log(alert)
+
+// // alert("Give Two Value In text input Boxes to Subtract these two Numbers..")
+
+// // var c3=Number(prompt("Write Any Number as the value of a"));
+// // var d4=Number(prompt("Write Any Number as the value of b"));
+// // var c=c3;
+// // var d=d4;
+// // var subtracting=c-d;
+
+// // alert(subtracting);
+
+// // alert("Give Two Value In text input Boxes to Multiplication these two Numbers..")
+
+// // var e5=Number(prompt("Write Any Number as the value of a"));
+// // var f6=Number(prompt("Write Any Number as the value of b"));
+// // var e=e5;
+// // var f=f6;
+// // var multiplication=e*f;
+
+// // alert(multiplication);
+
+// // alert("Give Two Value In text input Boxes to Division of these two Numbers..")
+
+// // var g5=Number(prompt("Write Any Number as the value of a"));
+// // var h6=Number(prompt("Write Any Number as the value of b"));
+// // var g=g5;
+// // var h=h6;
+// // var division=g/h;
+
+// // alert(division);
+
+// var time=Number(prompt("Write time in 24hours format"));
+// if(time>=0000 && time<1200){
+//     alert("Good Morning!")
+// }
+// else if(time>=1200 && time<1700){
+//     document.write("Good Afternoon!")
+// }
+// else if(time>=1700 && time<2100){
+//     document.write("Good evening!")
+// }
+// else if(time>=2100 && time<2359){
+//     document.write("Good Afternoon!")
+// }
+
+// var userName1st=prompt("Enter your first name \n Note: Remember for Password");
+// var userName2nd=prompt("Enter your last name \n Note: Remember for Password");
+// var password= userName1st+userName2nd;
+// var checkPasswrod=prompt("Enter Your Password \n Note: Your Password is your first and last name together with correct spelling.");
+// if(checkPasswrod===password){
+//     document.write("Your password is correct.")
+// }
+// else{
+//     document.write("Your password is Wrong. \n You are not " + userName1st);
+// }
+// document.write("<h4>" + "Hello " + userName1st + "</h4>");
+
+// // var monthEnter=prompt("Enter month number to check the month name. \n like 1=January");
+// // if(monthEnter==1){
+// //     alert("1st month name is called January.")
+// // }
+// // else if(monthEnter==2){
+// //     alert("2nd month name is called Febraury.")
+// // }
+// // else if(monthEnter==3){
+// //     alert("3rd month name is called March.")
+// // }
+// // else if(monthEnter==4){
+// //     alert("4th month name is called April.")
+// // }
+// // else if(monthEnter==5){
+// //     alert("5th month name is called May.")
+// // }
+// // else if(monthEnter==6){
+// //     alert("6th month name is called June.")
+// // }
+// // else if(monthEnter==7){
+// //     alert("7th month name is called July.")
+// // }
+// // else if(monthEnter==8){
+// //     alert("8th month name is called August.")
+// // }
+// // else if(monthEnter==9){
+// //     alert("9th month name is called September.")
+// // }
+// // else if(monthEnter==10){
+// //     alert("10th month name is called October.")
+// // }
+// // else if(monthEnter==11){
+// //     alert("11th month name is called November")
+// // }
+// // else if(monthEnter==12){
+// //     alert("12th month name is called December.")
+// // }
+// // else if(monthEnter>12){
+// //     alert("Totall Number of months are 12.. \n Try some less than 12 again...")
+// // }
+
+
+// Question no 1 
+var a = prompt("Enter your First name ");
+var b = prompt("Enter your Second name ");
+var fullName = (a  + b);
+document.write("<h1>" + "Assalam-O-Alaikum " + fullName, + "</h1>" + "<br>");
+
+
+// Question no 2
+var mobile = prompt("Enter your favorite mobile phone with model");
+document.write("Your favorite phone is:" + mobile, "<br>");
+document.write("Length of String:" + mobile.length, "<br>");
+
+// Question no 3
+var text = prompt("Enter your Nationality");
+for (i = 1; i < text.length; i++){
+    if (text.charAt(i) === "n"){
+        document.write("String:" + text, "<br>");
+        document.write("index of 'n': " + i, "<br>");
+    }
+}
+
+
+
+// Question no 5
+var index = text;
+var index2 = index.charAt(3);
+document.write("String:" + index, "<br>")
+document.write("Character at index 3:" + index2, "<br>");
+
+
+
+// Question no 6
+var firstName = a;
+var lastName = b;
+var fullName = firstName + lastName;
+document.write(fullName + " FullName using string concat() method.", "<br>");
+
+
+
+// Question no 7
+var city = "Hyderabad";
+var city2 = city.replace("Hyderabad", "Islamabad");
+document.write("City: " + city, "<br>");
+document.write("After replacement: " + city2, "<br>");
+
+
+
+// Question no 8
+var bestFriend = prompt("Enter your best friends Name.")
+var message = a + " and " + bestFriend + " are best friends. They play cricket and football together.";
+var message2 = message.replace(/and/g, "&");
+document.write("Message: " + message, "<br>");
+document.write("After replacement: " + message2, "<br>");
+
+
+
+
+// Question no 9
+// var no1 = "123";
+// var no2 = parseInt(no1);
+// document.write(no1.value(), "<br>");
+// document.write(no1.type(), "<br>");
+// document.write(no2.value(), "<br>");
+// document.write(no2.type(), "<br>");
+
+
+
+// Question no 10
+var url = prompt("Enter eb url. e.g: www.google.com");
+var url2 = (url + ".google.com");
+document.write(url2 + "<br>");
+
+
+
+// Question no 11
+var fruit = prompt("Enter your favorite Fruit name");
+document.write("User input: " + fruit, "<br>");
+var fruit2 = fruit.toUpperCase();
+document.write("UpperCase: " + fruit2, "<br>");
+
+
+
+// Question no 12
+var dryFruit = prompt("Enter your favorite DryFruit name in UpperCase format");
+document.write("User input: " + dryFruit, "<br>");
+var dryFruit2 = dryFruit.toLowerCase();
+document.write("LowerCase: " + dryFruit2, "<br>");
+
+
+
+
+// Question no 13
+var a = prompt("Write any Number");
+var b = parseInt(a);
+alert(b);
+
+
+
+// Question no 14
+var number = 35.36;
+var number2 = number.toString();
+document.write("Number: " + number, "<br>");
+document.write("Result: " + number2 * 100, "<br>");
+
+
+
+// Question no 15
+var x = "4";
+var y = "4";
+var z = x + y;
+document.write("x is " + x, "<br>");
+document.write("y is " + y, "<br>");
+document.write("x + y is " + z, "<br>");
+
+
+
+// Question no 16
+var d = "9";
+var e = "9";
+var f = parseInt(d) - parseInt(e);
+document.write("d is " + d, "<br>");
+document.write("e is " + e, "<br>");
+document.write("d - e is " + f, "<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
